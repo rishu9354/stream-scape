c8c66ecacc4cbd29e4570f4b6ecc6d515911a4ef
rishu9354 <rs682688@gmail.com>
2025-09-06 13:40:46 +0530

    update axios

--- app/plugins/axios.client.js
+++ app/plugins/axios.client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default defineNuxtPlugin(() => {
   const instance = axios.create({
-    baseURL: 'http://localhost:3001', // backend url
+    baseURL: 'https://backend-stream-scape.onrender.com/', // backend url
     withCredentials: true //cookies ke liye
   })
 
