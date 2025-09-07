4c370fc60efc8993e092aed47846f42b48bf5a14
rishu9354 <rs682688@gmail.com>
2025-09-07 17:02:10 +0530

    updated

--- app/plugins/axios.client.js
+++ app/plugins/axios.client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default defineNuxtPlugin(() => {
   const instance = axios.create({
-    baseURL: 'https://backend-stream-scape.onrender.com/', // backend url
+    baseURL: 'https://backend-stream-scape-production.up.railway.app/', // backend url
     withCredentials: true //cookies ke liye
   })
 
@@ -10,3 +10,7 @@ export default defineNuxtPlugin(() => {
     provide: { axios: instance }
   }
 })
+
+// backend url : https://backend-stream-scape.onrender.com/->render
+// backend url: https://backend-stream-scape-production.up.railway.app/ ->railway.app
+
